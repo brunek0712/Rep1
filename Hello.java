e666b96e5a5cd22c63dcdefe5ae9bedb560e4274
brunek748 <brunek748@cis5.strose.edu>
2017-02-01 11:10:11 -0500

    Hello!
    Looks Like added something to my Hello.java file.

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("What's up?");
+
+		System.out.println("Thanks for running, " + args[0] + "!");
+
+
+	}
+
+}
